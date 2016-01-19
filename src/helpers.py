19fcb950f6458f0c3ea056b63af81fd3c35dac6e
David Whale <david@thinkingbinaries.com>
2016-01-19 22:47:55 +0000

    Runtime support for self sizing 2D array added

--- src/helpers.py
+++ src/helpers.py
@@ -1,33 +1,54 @@
-class Cells():
+class Array():
     def __init__(self):
-        self.cells = []
+        self.data = []
 
     def __setitem__(self, index, value):
-        l = len(self.cells)
-        if index > l:
-            missing = 1+(index - l)
-            self.cells.extend([0 for i in range(missing)])
-        self.cells[index] = value
+        l = len(self.data)
+        if index >= l:
+            missing = 1+(index-l)
+            self.data.extend([0 for i in range(missing)])
+        self.data[index] = value
 
     def __getitem__(self, index):
-        if index > len(self.cells):
+        if index >= len(self.data):
             return 0 # lazy construction
-        return self.cells[index]
+        return self.data[index]
 
     def __len__(self):
-        return len(self.cells)
+        return len(self.data)
 
     def __repr__(self):
-        return str(self.cells)
+        return str(self.data)
+
+
+class Array2D():
+    def __init__(self):
+        self.rows = []
+
+    def __getitem__(self, index):
+        l = len(self.rows)
+        if index >= l:
+            missing = 1+(index-l)
+            self.rows.extend([Array() for i in range(missing)])
+        return self.rows[index]
+
+    def __repr__(self):
+        return str(self.rows)
+
 
 #TODO: helper for readline
 #TODO: helper for writeline
 
 def test():
-    a = Cells()
+    a = Array()
     a[20] = "Fred"
     print(a[20])
     print(a)
+
+    b = Array2D()
+    b[4][5]='45'
+    print(b[4][5])
+    print(b)
 
 if __name__ == "__main__":
     test()
