256b9952140caa2571f6b2ea79d1d2ceb29acc7d
David Whale <david@thinkingbinaries.com>
2016-01-25 19:37:56 +0000

    Array constructors coded and tested Ok

--- src/pygen.py
+++ src/pygen.py
@@ -4,15 +4,24 @@
 
 import sys
 
+class ParserException(Exception):
+    pass
+
+
 class Generator():
     def __init__(self):
-        self.indentlev = 0
+        self.indentlev   = 0
         self.nest_stack  = []
         self.case_stack  = []
         self.procfn      = 0
 
+        self.global_vars = []
+        self.local_vars  = None
+
         self.out("from io import *")
         self.out("from array import *")
+
+    #----- EMITTER FUNCTIONS --------------------------------------------------
 
     def indent(self):
         self.indentlev += 4
@@ -23,7 +32,66 @@ class Generator():
     def out(self, msg):
         print((" " * self.indentlev) + msg)
 
-    #----- GENERATED ON THE FLY -----------------------------------------------
+
+    #----- HELPERS ------------------------------------------------------------
+
+    def now_local(self):
+        """Call this when entering local scope, i.e. a fn or proc"""
+        self.local_vars = []
+
+    def now_global(self):
+        """Call this when returnign to global scope, i.e. returning from a fn or proc"""
+        self.local_vars = None
+
+    def var_exists(self, name):
+        """Does this variable exist?"""
+        if self.local_vars != None:
+            if name in self.local_vars: return True
+
+        if name in self.global_vars: return True
+        return False
+
+    def create_var(self, name):
+        """Create a variable in the correct scope"""
+        if self.local_vars != None:
+            self.local_vars.append(name)
+        else:
+            self.global_vars.append(name)
+
+
+    def copy(self, p, i_item):
+        item = p[i_item]
+        r = item
+        p[0] = r
+
+    def empty(self, p):
+        p[0] = ""
+
+    def comma(self, p, i_left, i_right):
+        left = p[i_left]
+        right = p[i_right]
+        try:
+            r = left + ", " + right
+        except TypeError:
+            r = str(left) + ", " + str(right)
+        p[0] = r
+
+    def concat(self, p, i_one, i_two):
+        one = p[i_one]
+        two = p[i_two]
+        try:
+            both = one + two
+        except TypeError:
+            both = str(one) + str(two)
+        p[0] = both
+
+    def statement(self, p):
+        if len(self.nest_stack) != 0:
+            # Increment counter of statements at this nest level
+            self.nest_stack[-1] += 1
+
+
+    #----- GENERATE OUTPUT ON THE FLY -----------------------------------------
 
     def OUTPUT(self, p, i_expr):
         expr = p[i_expr]
@@ -35,23 +103,41 @@ class Generator():
         self.out("%s = %s" % (id, expr))
 
     def array1assign(self, p, i_id, i_indexexpr, i_valueexpr):
+        """Write to a 1D array"""
         id = p[i_id]
         indexexpr = p[i_indexexpr]
         valueexpr = p[i_valueexpr]
-        #TODO arrays need to auto-size, for this to work
+        if not self.var_exists(id):
+            # construct an array object before we write to it
+            self.create_var(id)
+            self.out("%s = Array()" % id)
+
         self.out("%s[%s] = %s" % (id, indexexpr, valueexpr))
 
     def array2assign(self, p, i_id, i_index1expr, i_index2expr, i_valueexpr):
+        """Write to a 2D array"""
         id = p[i_id]
         index1expr = p[i_index1expr]
         index2expr = p[i_index2expr]
         valueexpr  = p[i_valueexpr]
-        #TODO arrays need to auto-size, for this to work
+
+        if not self.var_exists(id):
+            # construct a 2D array object before we write to it
+            self.create_var(id)
+            self.out("%s = Array2D()" % id)
+
         self.out("%s[%s][%s] = %s" % (id, index1expr, index2expr, valueexpr))
 
     def arrayinit(self, p, i_id, i_initialiser):
+        """Initialise a 1D array"""
         id = p[i_id]
         initialiser = p[i_initialiser]
+
+        if not self.var_exists(id):
+            # construct a 1D array object before we write to it
+            self.create_var(id)
+            self.out("%s = Array()" % id)
+
         self.out("%s = [%s]" % (id, initialiser))
 
     def READLINE(self, p, i_file, i_expr):
@@ -173,6 +259,7 @@ class Generator():
         self.indent()
         self.nest_stack.append(0) # count of statements in function
         self.procfn += 1
+        self.now_local()
 
     def RETURN(self, p, i_expr):
         expr = p[i_expr]
@@ -184,6 +271,7 @@ class Generator():
             self.out("pass")
         self.outdent()
         self.procfn -= 1
+        self.now_global()
 
     def PROCEDURE(self, p, i_id, i_params):
         if self.procfn != 0:
@@ -195,6 +283,7 @@ class Generator():
         self.indent()
         self.procfn += 1
         self.nest_stack.append(0) # count of statements in function
+        self.now_local()
 
     def ENDPROCEDURE(self, p):
         c = self.nest_stack.pop()
@@ -202,6 +291,7 @@ class Generator():
             self.out("pass")
         self.outdent()
         self.procfn -= 1
+        self.now_global()
 
     def callparams(self, p, i_params, i_expr):
         params = p[i_params]
@@ -352,53 +442,24 @@ class Generator():
         p[0] = r
 
     def array1dexpr(self, p, i_id, i_expr):
+        """Read a 1D array"""
         id   = p[i_id]
         expr = p[i_expr]
+        if not self.var_exists(id):
+            raise ParserException("Read from an array that does not exist")
         r = "%s[%s]" % (id, expr)
         p[0] = r
 
     def array2dexpr(self, p, i_id, i_expr1, i_expr2):
+        """Read a 2D array"""
         id   = p[i_id]
         expr1 = p[i_expr1]
         expr2 = p[i_expr2]
+        if not self.var_exists(id):
+            raise ParserException("Read from an array that does not exist")
         r = "%s[%s][%s]" % (id, expr1, expr2)
         p[0] = r
 
-    #----- HELPERS ------------------------------------------------------------
-
-    def copy(self, p, i_item):
-        item = p[i_item]
-        r = item
-        p[0] = r
-
-    def empty(self, p):
-        p[0] = ""
-
-    def comma(self, p, i_left, i_right):
-        left = p[i_left]
-        right = p[i_right]
-        try:
-            r = left + ", " + right
-        except TypeError:
-            r = str(left) + ", " + str(right)
-        p[0] = r
-
-    def concat(self, p, i_one, i_two):
-        one = p[i_one]
-        two = p[i_two]
-        try:
-            both = one + two
-        except TypeError:
-            both = str(one) + str(two)
-        p[0] = both
-
-    def statement(self, p):
-        #if self.procfn != None:
-        #    self.procfn += 1
-
-        if len(self.nest_stack) != 0:
-            # Increment counter of statements at this nest level
-            self.nest_stack[-1] += 1
 
 
 
