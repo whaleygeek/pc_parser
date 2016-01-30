9983accf7e46452c51e7cb47e366f06bf0581f43
David Whale <david@thinkingbinaries.com>
2016-01-30 15:52:37 +0000

    base->hexascii with new simpler signatures

--- src/runtime/hexascii.py
+++ src/runtime/hexascii.py
@@ -1,18 +1,14 @@
-# base.py  29/01/2016  D.J.Whale
+# hexascii.py  29/01/2016  D.J.Whale
 #
-# Provide simple base independent literals and conversions
+# Provide simple hex literals and conversions
 
-#TODO: rename module 'hexascii'
 
-# OUTPUT base_to_hexstr(42)
-# OUTPUT base_to_hexstr(32767, 4)
+# OUTPUT hexascii_tostr(42)
+# OUTPUT hexascii_tostr(32767, 4)
 
 import arrays
 
-#TODO: rename as tohexstr()
-#TODO: if bytes=None, should make it 2,4,6,8 chars depending on value.
-
-def base_to_hexstr(n, bytes=1):
+def hexascii_tostr(n, bytes=1):
     """convert n into a hex string, honoring the bytes as the max width.
     Note that varargs by default are supported, so this should work."""
     n = n & 0xFFFFFFFF # negative numbers will be two's complement
@@ -34,22 +30,18 @@ def base_to_hexstr(n, bytes=1):
         raise ValueError("Only bytes=[1..4] supported")
 
 
-# n <- base_hex("A3")
-# n <- base_hex("DEADBEEF")
+# n <- hexascii_fromstr("A3")
+# n <- hexascii_fromstr("DEADBEEF")
 
-#TODO: rename to fromhexstr()
-
-def base_hex(s):
+def hexascii_fromstr(s):
     """Convert s as a hex string into a number.
     Supports 1,2,3,4 byte numbers."""
     return int(s, base=16)
 
 
-# a <- base_hex_bytearray("A01237EC12120D0A")
+# a <- hexascii_array("A01237EC12120D0A")
 
-#TODO: rename as arrayfromhexstr()
-
-def base_hex_bytearray(s):
+def hexascii_array(s):
     """Convert s as a hex string into an array initialiser.
     This probably just needs to be a list of numbers, and make sure
     that the array initialiser will work with any expression which
